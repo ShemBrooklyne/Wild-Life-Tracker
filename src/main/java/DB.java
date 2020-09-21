93b6ff4110a43e90928ed1a63da9d460595a4108
ShemBrooklyne <shemnyatti@gmail.com>
2020-09-22 00:08:39 +0300

    .

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,13 @@
-import org.sql2o.*;
+import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "access", "Access");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "access", "Access");
+
+
+    String connectionString = "jdbc:postgresql://ec2-54-160-202-3.compute-1.amazonaws.com:5432/dss83t7hsl3hs"; //!
+    Sql2o sql2o = new Sql2o(connectionString, "ohvceqaxugrmnl", "c7c2f789e14d7cfda1ef75fab5adaa6a69fa713f1d651b18ff316873afa628d6"); //!
+
+
+//    postgres://ohvceqaxugrmnl:c7c2f789e14d7cfda1ef75fab5adaa6a69fa713f1d651b18ff316873afa628d6@ec2-54-160-202-3.compute-1.amazonaws.com:5432/dss83t7hsl3hs
+
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,20 +5,23 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "access", "Access");
+//        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "access", "Access");
+
+        String connectionString = "jdbc:postgresql://ec2-54-160-202-3.compute-1.amazonaws.com:5432/dss83t7hsl3hs"; //!
+        Sql2o sql2o = new Sql2o(connectionString, "ohvceqaxugrmnl", "c7c2f789e14d7cfda1ef75fab5adaa6a69fa713f1d651b18ff316873afa628d6"); //!
     }
 
-    @Override
-    protected void after() {
-        try(Connection con = DB.sql2o.open()) {
-            String deleteAnimalsQuery = "DELETE FROM animals *;";
-            String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
-            String deleteSightingsQuery = "DELETE FROM sightings *;";
-            con.createQuery(deleteAnimalsQuery).executeUpdate();
-            con.createQuery(deleteEndangeredAnimalsQuery).executeUpdate();
-            con.createQuery(deleteSightingsQuery).executeUpdate();
-        }
-    }
+//    @Override
+//    protected void after() {
+//        try(Connection con = DB.sql2o.open()) {
+//            String deleteAnimalsQuery = "DELETE FROM animals *;";
+//            String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
+//            String deleteSightingsQuery = "DELETE FROM sightings *;";
+//            con.createQuery(deleteAnimalsQuery).executeUpdate();
+//            con.createQuery(deleteEndangeredAnimalsQuery).executeUpdate();
+//            con.createQuery(deleteSightingsQuery).executeUpdate();
+//        }
+//    }
 
 
 }
